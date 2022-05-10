fb8bc862f3076c5f389a9d45180500c7f06b33c3
Bhotton25 <bhotton25@gmail.com>
2022-05-09 18:24:09 -0600

    Added signup link, because for some reason it disappeared.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,33 +1,37 @@
-const router = require('express').Router();
-const withAuth = require('../utils/auth');
-const path = require('path');
+const router = require("express").Router();
+const withAuth = require("../utils/auth");
+const path = require("path");
 
 //All Necessary pages with authorization as needed
 
-router.get('/', (req, res) => {
-      res.render('homepage', {
-           loggedIn: req.session.loggedIn
-   });
+router.get("/", (req, res) => {
+  res.render("homepage", {
+    loggedIn: req.session.loggedIn,
+  });
 });
 
-router.get('/login', (req,res) => {
-    res.render('login');
- });
-
-router.get('/office', withAuth, (req,res) => {
-        res.render('office');
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-router.get('/warehouse', withAuth, (req,res) => {
-        res.render('warehouse');
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+router.get("/office", withAuth, (req, res) => {
+  res.render("office");
+});
+
+router.get("/warehouse", withAuth, (req, res) => {
+  res.render("warehouse");
 });
 
 router.get("/completed", withAuth, (req, res) => {
   res.render("completed");
 });
 
-router.get('/request', (req,res) => {
-    res.sendFile(path.join(__dirname, '../public/request.html'));
+router.get("/request", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/request.html"));
 });
 
 module.exports = router;
